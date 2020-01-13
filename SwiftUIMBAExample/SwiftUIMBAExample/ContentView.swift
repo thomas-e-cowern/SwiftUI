b503d4cdf48148e3b1baea5baf7ab5626008e72a
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-01-13 11:37:15 -0500

    added images and text

--- SwiftUIMBAExample/SwiftUIMBAExample/ContentView.swift
+++ SwiftUIMBAExample/SwiftUIMBAExample/ContentView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Image("gs").resizable().aspectRatio(contentMode: .fit)
+            Image("steph").resizable().aspectRatio(contentMode: .fit)
+            Text("Steph Curry")
+            Spacer()
+        }.edgesIgnoringSafeArea(.top)
     }
 }
 
