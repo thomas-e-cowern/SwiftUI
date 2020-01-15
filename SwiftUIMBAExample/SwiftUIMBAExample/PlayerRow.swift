d37243d4ac703425b8bd574e06e97fe84dd101b1
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-01-15 15:01:37 -0500

    updted text size on row

--- SwiftUIMBAExample/SwiftUIMBAExample/PlayerRow.swift
+++ SwiftUIMBAExample/SwiftUIMBAExample/PlayerRow.swift
@@ -17,9 +17,9 @@ struct PlayerRow: View {
     var body: some View {
         
         HStack {Image(player.imageName).resizable().aspectRatio(contentMode: .fit).clipShape(Circle()).background(Circle()).foregroundColor(player.team.color).overlay(Circle().stroke(Color.white, lineWidth: 4))
-            Text(player.name).font(.largeTitle)
+            Text(player.name).font(.largeTitle).lineLimit(1).minimumScaleFactor(0.6)
             Spacer()
-        }
+        }.minimumScaleFactor(0.6)
     }
 }
 
