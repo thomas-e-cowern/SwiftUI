366c610daa1ae8435657f6588882d76465ca7811
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-01-15 12:10:31 -0500

    fixed player name break

--- SwiftUIMBAExample/SwiftUIMBAExample/ContentView.swift
+++ SwiftUIMBAExample/SwiftUIMBAExample/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
             Image(player.team.imageName).resizable().aspectRatio(contentMode: .fit)
             Image(player.imageName).clipShape(Circle()).background(Circle()).foregroundColor(.white).overlay(Circle().stroke(Color.white, lineWidth: 4)).offset(x: 0, y: -90).padding(.bottom, -90).shadow(radius: 15)
             
-            Text(player.name).font(.system(size: 50)).fontWeight(.heavy)
+            Text(player.name).font(.system(size: 50)).fontWeight(.heavy).minimumScaleFactor(0.6)
             
             CapitalStatText(statName: "Age", statValue: "\(player.age)")
             CapitalStatText(statName: "Height", statValue: player.height)
@@ -31,7 +31,9 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        
-        ContentView(player: players[1]).previewDevice("iPhone 11")
+        Group {
+            ContentView(player: players[2]).environment(\.sizeCategory, .extraLarge).previewDevice("iPhone 8")
+            ContentView(player: players[2]).environment(\.sizeCategory, .extraLarge).previewDevice("iPhone SE")
+        }
     }
 }
