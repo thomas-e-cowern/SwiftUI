c80a182e0ccc829cedbdfde06486433f598442d1
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-01-28 15:06:28 -0500

    initial commit

---
+++ BitPriceTest/BitPriceTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BitPriceTest
+//
+//  Created by Thomas Cowern New on 1/28/20.
+//  Copyright © 2020 Thomas Cowern New. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
