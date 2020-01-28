41e0ea060e7bcd54c65b0484503668b76e1e524f
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-01-28 10:18:40 -0500

    added gradient

---
+++ Animation/Animation/Funny.swift
@@ -0,0 +1,30 @@
+//
+//  Funny.swift
+//  Animation
+//
+//  Created by Thomas Cowern New on 1/28/20.
+//  Copyright © 2020 Thomas Cowern New. All rights reserved.
+//
+
+import SwiftUI
+
+struct Funny: View {
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                let width = geometry.size.width
+                let height = geometry.size.height
+                path.move(to: CGPoint(x: 0, y: 0))
+                path.addLine(to: CGPoint(x: 0, y: height))
+                path.addLine(to: CGPoint(x: width, y: height))
+                path.addLine(to: CGPoint(x: width, y: 0))
+            }.fill(LinearGradient(gradient: .init(colors: [.red, .yellow, .orange]), startPoint: .leading, endPoint: .trailing))
+        }
+    }
+}
+
+struct Funny_Previews: PreviewProvider {
+    static var previews: some View {
+        Funny()
+    }
+}
